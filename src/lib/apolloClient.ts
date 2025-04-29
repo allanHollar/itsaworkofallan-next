892b48239aa843af460ebc68bf03268361243e5e
allanHollar <allan@hollar.com>
2025-04-29 15:19:45 -0700

    Remove @apollo/client

--- src/lib/apolloClient.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-  uri: "https://us-west-2.cdn.hygraph.com/content/cm9lfoiu3004907wal31udt6s/master",
-  cache: new InMemoryCache(),
-});
