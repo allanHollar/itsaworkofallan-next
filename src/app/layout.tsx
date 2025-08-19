b4523968f83cdaaa0534f6a9d80dcee1bb974c34
allanr711 <allanr711@gmail.com>
2025-08-19 16:14:48 -0700

    Fix analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -135,6 +135,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preload" as="image" href={`${cdnBaseUrl}/footer.webp`} />
+
+        {/* GTM loader */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5D8FZR');
+          `}
+        </Script>
       </head>
       <body>
         <Providers>
